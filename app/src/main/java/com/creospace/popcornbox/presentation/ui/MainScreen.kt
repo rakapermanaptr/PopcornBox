25620ebbdcde058d8c938605d75ff37da1b7dc2f
Raka Permana <raka@digivest.id>
2025-03-15 11:49:52 +0700

    refactor: cleanup code

--- app/src/main/java/com/creospace/popcornbox/presentation/ui/MainScreen.kt
+++ app/src/main/java/com/creospace/popcornbox/presentation/ui/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.creospace.popcornbox.presentation.ui
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
@@ -19,15 +20,18 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
+import com.creospace.popcornbox.domain.model.Movie
 import kotlinx.coroutines.launch
 
 @Composable
 fun MainScreen(viewModel: MainViewModel = hiltViewModel(), paddingValues: PaddingValues) {
     val state by viewModel.state.collectAsState()
     val scope = rememberCoroutineScope()
+    val context = LocalContext.current
 
     LaunchedEffect(Unit) {
         scope.launch {
@@ -35,14 +39,23 @@ fun MainScreen(viewModel: MainViewModel = hiltViewModel(), paddingValues: Paddin
         }
     }
 
-    state.nowPlayingMovies?.let { nowPlayingMovies ->
+    when {
+        state.isLoading -> LoadingView()
+        state.errorMessage != null -> Toast.makeText(context, state.errorMessage, Toast.LENGTH_SHORT).show()
+        else -> NowPlayingMoviesView(nowPlayingMovies = state.nowPlayingMovies)
+    }
+}
+
+@Composable
+fun NowPlayingMoviesView(nowPlayingMovies: List<Movie>?) {
+    nowPlayingMovies?.let { movies ->
         LazyVerticalStaggeredGrid(
             columns = StaggeredGridCells.Adaptive(200.dp),
             verticalItemSpacing = 4.dp,
             horizontalArrangement = Arrangement.spacedBy(4.dp)
         ) {
-            items(nowPlayingMovies.size) { index ->
-                val movie = nowPlayingMovies[index]
+            items(movies.size) { index ->
+                val movie = movies[index]
                 AsyncImage(
                     model = movie.imageUrl,
                     contentScale = ContentScale.Crop,
@@ -54,14 +67,10 @@ fun MainScreen(viewModel: MainViewModel = hiltViewModel(), paddingValues: Paddin
             }
         }
     }
-
-    if (state.isLoading) {
-        Loading()
-    }
 }
 
 @Composable
-fun Loading() {
+fun LoadingView() {
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
